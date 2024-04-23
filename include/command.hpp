e2ad4bba5e9632c8cb2e6c229b9f61a3a4c67d7c
Philipp66904 <philippg.pgb@gmail.com>
2024-04-23 12:20:14 +0200

    removed old code

--- include/command.hpp
+++ include/command.hpp
@@ -45,14 +45,3 @@ namespace Command
         }
     };
 }
-
-// struct waypoint_command
-// {
-//     std::string target_coordinate;
-//     uint32_t pre_wait_time_ms = 0;
-//     uint32_t post_wait_time_ms = 0;
-//     float travel_height;
-//     float target_height;
-//     float horizontal_speed;
-//     float vertical_speed;
-// };
