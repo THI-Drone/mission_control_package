b342773b7faadccd8c7c577282047db1eb040958
MatixLoc <115873997+MatixLoc@users.noreply.github.com>
2024-04-24 17:08:45 +0200

    implement_Workflow (#4)
    * implement_Workflow
    
    * clang-format

--- include/event_loop_guard.hpp
+++ include/event_loop_guard.hpp
@@ -10,23 +10,25 @@
  * and set the flag to true when the instance is destroyed.
  * @note Doesn't take ownership of the pointer event_loop_activeFlag
  */
-class EventLoopGuard
-{
-private:
+class EventLoopGuard {
+   private:
     bool* event_loop_active;
     bool event_loop_active_init;
 
-public:
+   public:
     /**
      * @brief Constructs an EventLoopGuard object.
      *
-     * This constructor initializes an EventLoopGuard object with the given parameters.
+     * This constructor initializes an EventLoopGuard object with the given
+     * parameters.
      *
-     * @param event_loop_activeFlag A pointer to the active flag of the event loop.
-     * @param new_state The new state to set for the event loop. Will revert back to the opposite state on destruction.
+     * @param event_loop_activeFlag A pointer to the active flag of the event
+     * loop.
+     * @param new_state The new state to set for the event loop. Will revert
+     * back to the opposite state on destruction.
      */
-    EventLoopGuard(bool* event_loop_activeFlag, bool new_state) : event_loop_active(event_loop_activeFlag)
-    {
+    EventLoopGuard(bool* event_loop_activeFlag, bool new_state)
+        : event_loop_active(event_loop_activeFlag) {
         event_loop_active = event_loop_activeFlag;
         *event_loop_active = new_state;
 
@@ -35,16 +37,15 @@ public:
 
     /**
      * @brief Destructor for the EventLoopGuard class.
-     * 
-     * This destructor is responsible for updating the value of the event loop active flag.
-     * It sets the value of the event loop active flag to the negation of the initial event loop active flag.
-     * 
-     * @note This destructor assumes that the event loop active flag is a pointer to a boolean variable.
-     * 
+     *
+     * This destructor is responsible for updating the value of the event loop
+     * active flag. It sets the value of the event loop active flag to the
+     * negation of the initial event loop active flag.
+     *
+     * @note This destructor assumes that the event loop active flag is a
+     * pointer to a boolean variable.
+     *
      * @see EventLoopGuard
      */
-    ~EventLoopGuard()
-    {
-        *event_loop_active = !event_loop_active_init;
-    }
+    ~EventLoopGuard() { *event_loop_active = !event_loop_active_init; }
 };
--- include/structs.hpp
+++ include/structs.hpp
@@ -5,8 +5,7 @@
 /**
  * @brief Struct representing a heartbeat payload.
  */
-struct heartbeat_payload
-{
+struct heartbeat_payload {
     bool received; /**< Flag indicating if the payload has been received. */
     uint32_t tick; /**< The tick value of the payload. */
 
@@ -14,15 +13,13 @@ struct heartbeat_payload
      * @brief Default constructor for heartbeat_payload.
      * Initializes the received flag to false and the tick value to 0.
      */
-    heartbeat_payload()
-    {
+    heartbeat_payload() {
         received = false;
         tick = 0;
     }
 };
 
-struct ros_node
-{
+struct ros_node {
     struct heartbeat_payload hb_payload;
     bool can_start_mission = false;
 };
