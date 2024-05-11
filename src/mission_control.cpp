44eca2eb5cabaf6c2365f1930d33f16202c9902a
Philipp66904 <philippg.pgb@gmail.com>
2024-05-11 17:50:28 +0200

    explicitly deactivating active node on mission finished

--- src/mission_control.cpp
+++ src/mission_control.cpp
@@ -278,6 +278,18 @@ void MissionControl::mission_finished() {
     RCLCPP_INFO(this->get_logger(),
                 "MissionControl::%s: Mission finished successfully", __func__);
 
+    // Explicitly deactivate currently active node (if not mission control or
+    // empty)
+    if (get_active_node_id().size() > 0 &&
+        get_active_node_id() != this->get_name()) {
+        send_control(get_active_node_id(), false, "");
+
+        RCLCPP_INFO(
+            this->get_logger(),
+            "MissionControl::%s: Explicitly deactivated active node: '%s'",
+            __func__, get_active_node_id().c_str());
+    }
+
     // Reset internal state
     clear_active_node_id();
 
